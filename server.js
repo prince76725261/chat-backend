0ebb890564da3daa5f19752aa0d7d8c0aff32567
Prince <bediprince62@gmail.com>
2023-08-21 14:18:41 +0530

    allow all origin

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ const server = app.listen(
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000", // Update with your frontend URL
+    origin: "*", // Update with your frontend URL
   },
 });
 
